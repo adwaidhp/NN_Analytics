de151321bc094aae1de21684a26061f63633aedc
Varunpgeorge <145317869+Varunpgeorge@users.noreply.github.com>
2025-10-15 23:42:58 +0530

    Delete Assignments/ML S7 CSE/libNN directory

--- Assignments/ML S7 CSE/libNN/global_enum.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GLOBAL_ENUM_H__
-#define __GLOBAL_ENUM_H__
-
-// Added LINEAR and renamed TANH_ACT to avoid conflict with std::tanh
-enum class ACT_FUN { LINEAR, SIGMOID, RELU, TANH_ACT, LEAKY_RELU };
-
-#endif
