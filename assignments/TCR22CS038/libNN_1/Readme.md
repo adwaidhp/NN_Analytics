e9dd4380ad5b3ff55462a1172034ec66e8240c93
Johns-43 <johnsandrews9725@gamil.com>
2025-10-16 23:34:15 +0530

    modified

--- assignments/TCR22CS038/libNN_1/Readme.md
+++
@@ -1,3 +0,0 @@
-Changes:
-changed the learning rate from 0.000001 to 0.001
-changed the epochs
--- global_enum.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GLOBAL_ENUM_H__
-#define __GLOBAL_ENUM_H__
-
-// Renamed tanh to tanh_act to avoid conflict with std::tanh
-enum ACT_FUN { sigmoid, tanh_act, relu, leaky_relu };
-
-#endif
