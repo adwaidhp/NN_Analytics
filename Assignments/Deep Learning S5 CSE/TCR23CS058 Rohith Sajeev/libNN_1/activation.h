020bdd55030db3856f9209bc67b3ea7ac519a464
rohith1612 <im.rohithsajeev@gmail.com>
2025-10-27 00:22:50 +0530

    after training

---
+++ Assignments/Deep Learning S5 CSE/TCR23CS058 Rohith Sajeev/libNN_1/activation.h
@@ -0,0 +1,73 @@
+#ifndef __ACTIVATION_H__
+#define __ACTIVATION_H__
+
+#include "global_enum.h"
+#include <cmath>
+
+class activation 
+{
+	public:
+	
+		activation ()
+		{
+		
+		}
+	
+		double function (ACT_FUN id, double x) 
+		{
+			switch (id)
+			{
+				case ACT_FUN::sigmoid:
+				
+					return sigmoid (x);
+				
+				case ACT_FUN::relu:
+				
+					return relu (x);
+				
+				case ACT_FUN::tanht:
+				
+					return tanh (x);
+				
+				case ACT_FUN::leaky_relu:
+				
+					return leaky_relu (x);
+					
+				default:
+				
+					return x;
+			}		
+		}
+	
+		double sigmoid (double x)
+		{
+			return 1.0 / (1.0 + std::exp(-x));
+		}
+
+		double relu (double x)
+		{
+			if (x > 0.0) 
+			{
+				return x;
+			}
+		
+			return 0.0;
+		}
+
+		double leaky_relu (double x)
+		{
+			if (x > 0.0)
+			{
+				return x;
+			}
+			
+			return 0.01 * x;
+		}
+		
+		double tanht (double x)
+		{
+			return std::tanh(x);
+		}
+};
+
+#endif
---
+++ Assignments/Deep Learning S5 CSE/TCR23CS058 Rohith Sajeev/libNN_1/global_enum.h
@@ -0,0 +1,6 @@
+#ifndef __GLOBAL_ENUM_H__
+#define __GLOBAL_ENUM_H__
+
+enum ACT_FUN {sigmoid, tanht, relu, leaky_relu};
+
+#endif
