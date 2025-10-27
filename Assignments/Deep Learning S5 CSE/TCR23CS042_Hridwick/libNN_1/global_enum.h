d7a6f62ab783307ecc72062bcc5bb8b9c3f6edbd
vrhridwick <vrhridwick@gmail.com>
2025-10-27 15:24:55 +0000

    Updates made to the model

---
+++ Assignments/Deep Learning S5 CSE/TCR23CS042_Hridwick/libNN_1/global_enum.h
@@ -0,0 +1,12 @@
+#ifndef __GLOBAL_ENUM_H__
+#define __GLOBAL_ENUM_H__
+
+enum ACT_FUN {sigmoid, tanh_act, relu, leaky_relu};
+
+#endif
+// enum ACT_FUN {
+//     ACT_SIGMOID,
+//     ACT_TANH,
+//     ACT_RELU,
+//     ACT_LEAKY_RELU
+// };
